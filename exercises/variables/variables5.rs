4f34354e191ed017058868bb6e0d4b3c4a9f5e48
KishanDahiya <kkd999.km@gmail.com>
2023-06-29 08:53:35 +0530

    chore : Done with variables

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,19 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    // {
+    //     //Shadowing and scope concept
+    //     //This below is scope concept, applying it to innerblock
+    //     let number:u32 = 3; // don't rename this variable
+    //     println!("Number plus two is : {}", number + 2);    
+    // }
+    //This below is shadowing conecpt applied to outer block on same variable
+    //Once we have reassigned the variable by not initializing but declaring it will erase previous value 
+    // from its memoery stack(Buffer) & update it with new value
+    let number:u32 = 3; // don't rename this variable
+    println!("Number plus two is : {}", number + 2);    
 }
