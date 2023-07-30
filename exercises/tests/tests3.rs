a8b49b79a49be06cdcc3ba167e3e1fc1a28f004e
KishanDahiya <kkd999.km@gmail.com>
2023-07-30 20:18:47 +0530

    chore: done tests

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,11 +7,40 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn is_true_when_even() {
+//         assert!(true);
+//     }
+
+//     #[test]
+//     fn is_false_when_odd() {
+//         assert!(true);
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn is_true_when_even() {
+//         assert!(is_even(2));
+//     }
+
+//     #[test]
+//     fn is_false_when_odd() {
+//         assert!(is_even(2));
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -19,11 +48,12 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let result = is_even(2);
+        assert_eq!(result, true);
     }
 
-    #[test]
     fn is_false_when_odd() {
-        assert!();
+        let result = is_even(5);
+        assert_eq!(result, false);
     }
 }
